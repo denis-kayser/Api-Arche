b76ab608ee44870727ea75b01021f34e7978889c
Denis <denis.herrera@kayser.pe>
2026-04-10 08:25:04 -0500

    cambio de env API_PORT

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { HOST, PORT } from './config/config'
+import { PORT } from './config/config'
 import { authMiddleware } from './middleware/middleware'
 import { errorHandler } from './middleware/errorHandler'
 
@@ -27,6 +27,6 @@ app.use('/api/v1', userRouter); // usuarios
 // Middleware de errores
 app.use(errorHandler)
 
-app.listen(PORT, HOST, () => {
-  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,6 @@ import { config } from 'dotenv'
 config()
 
 export const PORT: number = Number(process.env.PORT) || 5000
-export const HOST: string = process.env.HOST!
 
 export const DB_HOST: string = process.env.DB_HOST!
 export const DB_PORT: number = Number(process.env.DB_PORT) || 5432
